628b9247dfde339307b633af5bd8b9b97274410a
Antonio Pantaleo <46167308+antoniopantaleo@users.noreply.github.com>
2021-08-13 11:40:44 +0200

    Initial Commit

---
+++ Rick And Morty/Rick_And_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_And_MortyApp.swift
+//  Rick And Morty
+//
+//  Created by Antonio Pantaleo on 13/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Rick_And_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rick And MortyTests/Rick_And_MortyTests.swift
@@ -0,0 +1,33 @@
+//
+//  Rick_And_MortyTests.swift
+//  Rick And MortyTests
+//
+//  Created by Antonio Pantaleo on 13/08/21.
+//
+
+import XCTest
+@testable import Rick_And_Morty
+
+class Rick_And_MortyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
